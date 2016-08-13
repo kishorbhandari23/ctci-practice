3487a2d5d29cff2215a74822efe01390966342d9
kishor <info.kishorbhandari@gmail.com>
2016-08-13 12:47:27 +0530

    added some comments in q7

--- chap1/q5.cpp
+++ chap1/q5.cpp
@@ -6,34 +6,29 @@ using namespace std;
 bool one_edit_away(string str1, string str2)
 {
 	int match=0,i=0;
-	int str1_len, str2_len,big_len;
-	str1_len = str1.length();
-	str2_len = str2.length();
+	int str1_len, str2_len;
 
-
-	if (abs(str1_len - str2_len) > 1)
+	if (abs(str1_length() - str2_length()) > 1)
 		return false;
 
-	if (str2_len > str1_len) {
+	if (str2_len > str1_len) //take str1 as bigger string
 		swap(str1, str2);
-		swap(str1_len, str2_len);
-	}
 
-	while ((str1[i] == str2[i])&&(i<str2_len))
+	while ((str1[i] == str2[i])&&(i<str2_len)) //till the matches are found continue ad increase match 
 	{
 		match++,i++;
 	}
 
-	while ((str1[i+1] == str2[i]) && ((i+1)<str1_len))
+	while ((str1[i+1] == str2[i]) && ((i+1)<str1_len)) //Now see if leaving one character in bigger string , all other characters are equal
 	{
 		match++, i++;
 	}
-	while (((i + 1)<str2_len) && (str1[i+1] == str2[i+1])) //for pale & bale, kind of string
+	while (((i + 1)<str2_len) && (str1[i+1] == str2[i+1])) //for pale & bale, kind of string, everything except one charcter must be equal
 	{
 		match++, i++;
 	}
 
-	if ((str1_len - match) <= 1)
+	if ((str1_len - match) <= 1) 
 		return true;
 	else
 		return false;
